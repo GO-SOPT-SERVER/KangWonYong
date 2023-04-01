4a7537a76afe60093bf91a4d6d9723a65b20a672
KWY <wodrk1001@gmail.com>
2023-04-01 16:34:50 +0900

    init project

---
+++ src/main/java/com/sopt/kwy/soptseminar/SoptSeminarApplication.java
@@ -0,0 +1,13 @@
+package com.sopt.kwy.soptseminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoptSeminarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoptSeminarApplication.class, args);
+	}
+
+}
