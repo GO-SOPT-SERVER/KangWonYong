472142d5e2ca23f0666776c05a99ab369bc917b7
KWY <wodrk1001@gmail.com>
2023-04-26 13:21:31 +0900

    / 추가

--- src/main/java/com/sopt/kwy/soptseminar/controller/MemberController.java
+++ src/main/java/com/sopt/kwy/soptseminar/controller/MemberController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("member")
+@RequestMapping("/member")
 public class MemberController {
     @GetMapping("/{memberId}")
     public String hello(@PathVariable final Long memberId) {
