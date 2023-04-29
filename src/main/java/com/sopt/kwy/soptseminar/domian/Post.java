11c836e142d9a5ba51ca3bd4719e5d7f772434b5
KWY <wodrk1001@gmail.com>
2023-04-29 18:01:50 +0900

    [feat] domain 추가

---
+++ src/main/java/com/sopt/kwy/soptseminar/domian/Post.java
@@ -0,0 +1,28 @@
+package com.sopt.kwy.soptseminar.domian;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String content;
+    @Column(nullable = false)
+    private Date createdAt;
+    @ManyToOne
+    @JoinColumn(name = "user")
+    private User user;
+}
